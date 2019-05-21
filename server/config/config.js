0b063db5c0194520cdc5c4f07a5c3ea86cae1090
pablos <pablo.schrut@ngahr.com>
2019-05-21 23:05:10 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev'){
 	urlDB = 'mongodb://localhost:27018/cafe';
 } else {
-	urlDB = 'mongodb+srv://pschrut:fwfRSjS0tcBlCUHa@cluster0-g4cpb.mongodb.net/cafe';	
+	urlDB = process.env.MONGO_URI;	
 }
 
 process.env.URL_DB = urlDB;
